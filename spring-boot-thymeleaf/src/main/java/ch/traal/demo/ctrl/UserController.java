65dd90bf33152637f3a8f0129f80bfb95181d023
devel@traal.ch <devel@traal.ch>
2019-10-28 23:59:09 +0100

    chore: First revision.

---
+++ spring-boot-thymeleaf/src/main/java/ch/traal/demo/ctrl/UserController.java
@@ -0,0 +1,17 @@
+package ch.traal.demo.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+  /* methods */
+  @RequestMapping("demo")
+  public String demo(Model model) {
+    model.addAttribute("message", "Hello Thymeleaf");
+    // return to templates/demo.html page.
+    return "demo";
+  }
+}
