a56b8429b684001337b5c0a36199459876f6a662
devel@traal.ch <devel@traal.ch>
2019-10-29 18:38:07 +0100

    chore: First revision.

--- spring-boot-thymeleaf/src/main/java/ch/traal/demo/ctrl/UserController.java
+++ spring-boot-thymeleaf/src/main/java/ch/traal/demo/ctrl/UserController.java
@@ -1,8 +1,15 @@
 package ch.traal.demo.ctrl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.traal.demo.model.User;
 
 @Controller
 public class UserController {
@@ -11,7 +18,58 @@ public class UserController {
   @RequestMapping("demo")
   public String demo(Model model) {
     model.addAttribute("message", "Hello Thymeleaf");
+    double grade = 90.5;
+    model.addAttribute("grade", grade);
+    model.addAttribute("GPA", convertGPA(grade));
     // return to templates/demo.html page.
+
     return "demo";
+  }
+
+  private String convertGPA(double grade) {
+    if (grade >= 90) {
+      return "A";
+    } else if (grade < 90 && grade >= 80) {
+      return "B";
+    } else if (grade < 80 && grade >= 70) {
+      return "C";
+    } else if (grade < 70 && grade >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+
+  @RequestMapping("demo2")
+  public String demo2(Model model) {
+    List<User> lst = new ArrayList<>();
+    lst.add(new User(1, "Tom", 30));
+    lst.add(new User(2, "Jerry", 29));
+    lst.add(new User(3, "Nancy", 27));
+    model.addAttribute("list", lst);
+    return "demo2";
+  }
+
+  @RequestMapping("demo3")
+  public String demo3(HttpServletRequest request, Model model) {
+    // Add "request data" string to the request scope and associated it with the key
+    // "request" (we'll use the key value to retrieve the data)
+    request.setAttribute("request", "request data");
+
+    // Add "session data" String to the session scope and associate it with the key
+    // "session" (we'll use the key value to retrieve the data)
+    request.getSession().setAttribute("session", "session data");
+
+    // Add "application data" String to the application scope and associate it with
+    // the key "application" (we'll use the key value to retrieve the data)
+    request.getSession().getServletContext().setAttribute("application", "application data");
+    
+    return "demo3";
+  }
+  
+  @RequestMapping("demo4")
+  public String demo4(HttpServletRequest request, Model model) {
+    request.getSession().setAttribute("session", "session data");
+    return "demo4"; 
   }
 }
---
+++ spring-boot-thymeleaf/src/main/java/ch/traal/demo/model/User.java
@@ -0,0 +1,20 @@
+package ch.traal.demo.model;
+
+public class User {
+
+  
+  /* member variables */
+  public Integer id;
+  public String name;
+  public Integer age;
+
+  
+  /* constructors */
+  public User(Integer id, String name, Integer age) {
+      this.id = id;
+      this.name = name;
+      this.age = age;
+  }
+  
+  
+}
